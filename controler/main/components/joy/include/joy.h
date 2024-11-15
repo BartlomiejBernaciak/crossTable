6cf784e14d818f3162c390cea19a46f280a0d732
Bartlomiej Bernaciak <bartlomiej.bernaciak@pl.abb.com>
2024-11-15 16:13:28 +0100

    Heder file joy definitions

---
+++ controler/main/components/joy/include/joy.h
@@ -0,0 +1,41 @@
+#ifndef JOY_H
+#define JOY_H
+
+#include "inttypes.h"
+
+typedef enum{ 
+    x_axis,
+    y_axis,
+    z_axis,
+    w_axis,
+    e_axis,
+}joy_axis;
+
+typedef enum{
+    trigger,
+    left,
+    rignt,
+    side,
+    arrow_up,
+    arrow_down,
+    arrow_rignt,
+    arrow_left,
+}joy_btn;
+
+typedef struct {
+
+    int32_t axis[5];
+    uint8_t button[8];    
+
+
+    void *process;
+}joy_h;
+
+void set_axis(joy_h *h, joy_axis axis);
+void get_axis(joy_axis axis);
+
+void get_btn(joy_h *h, joy_btn);
+void set_btn(joy_h *h, joy_btn);
+
+
+#endif
---
+++ controler/main/main.c
@@ -0,0 +1,55 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "string.h"
+
+
+
+
+/**
+ * @brief This is takk responsable for displaying menu on the screen
+ *
+ *
+ *
+ */
+
+void lcd_task(){
+
+}
+
+
+/**
+    *   this will be task function that  is respononcability to 
+    *   @brief - 
+    *
+    */
+
+void joy_task(){
+
+
+
+
+}
+
+static const char *TAG = "Main";
+void app_main(void)
+{
+    
+    ESP_LOGI(TAG, "Starting controler");
+    
+
+    
+
+}
